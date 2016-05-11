378cc968c4b6d2cc1dc87f4f1e7bca33deb49c1c
José Márquez <josemarq@yahoo.com>
2016-05-11 22:54:26 +0200

    Create pull.php

---
+++ pull.php
@@ -0,0 +1,19 @@
+<?php
+
+$frames = array( "frames" => array(
+        array(
+            'index' => 0,
+            'text' => "Text1",
+            'icon' => "a87"
+        ),
+        array(
+            'index' => 1,
+            'text' => "Text2",
+            'icon' => "a87"
+        
+        )   
+    )
+);
+echo json_encode ($frames);
+
+?>
